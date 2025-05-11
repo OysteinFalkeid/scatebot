99f53d8df789db92adbb8672c4237164f07e59e4
Øystein <oystein.fal@gmail.com>
2025-05-11 22:42:14 +0200

    some cleanupp and adding arduino compiler

---
+++ arduino/led_blink_UART/led_blink_UART.ino
@@ -0,0 +1,52 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+
+#define F_CPU 16000000
+#define ARDUINO 10805
+
+
+#define LED_PIN PB5  // Arduino Uno: pin 13 = PORTB5
+
+uint8_t ticks = 0;
+
+void setupTimer0() {
+  TCCR0A  = (1 << WGM01);               // CTC mode
+  TCCR0B = (1 << CS02) | (1 << CS00);  // Prescaler = 1024
+  OCR0A = 255;                         // Compare match value
+  TIMSK0 = (1 << OCIE0A);              // Enable Timer1 compare match interrupt A
+}
+
+ISR(TIMER0_COMPA_vect) {
+  ticks++;
+  if (ticks >= 1) {                     // ~1 second
+    PORTB ^= (1 << LED_PIN);             // Toggle LED
+    ticks = 0;
+  }
+}
+
+int main(void) {
+  cli();
+
+  // Set LED pin as output
+  DDRB |= (1 << LED_PIN);
+
+  // Disable unused peripherals
+  power_adc_disable();
+  power_spi_disable();
+  power_timer1_disable();
+  power_twi_disable();
+
+  // Enable Timer0 only
+  power_timer0_enable();
+
+  setupTimer0();
+
+  sei();
+
+  while (1) {
+    //idel
+    sleep_mode();
+  }
+}
--- ros2_ws/build/gamepad_joy/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/oystein/scatebot/ros2_ws/install/gamepad_joy'
