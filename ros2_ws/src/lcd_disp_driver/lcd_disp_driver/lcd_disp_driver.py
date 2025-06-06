ed33fe64218c14464a2f249945f59363632d148d
Øystein <oystein.fal@gmail.com>
2025-06-06 22:14:55 +0200

    fixed bug in lcd display

--- ros2_ws/src/lcd_disp_driver/lcd_disp_driver/lcd_disp_driver.py
+++ ros2_ws/src/lcd_disp_driver/lcd_disp_driver/lcd_disp_driver.py
@@ -15,6 +15,9 @@ class LCS_DISP_DRIVER(Node):
     def __init__(self, node_name, *, context = None, cli_args = None, namespace = None, use_global_arguments = True, enable_rosout = True, start_parameter_services = True, parameter_overrides = None, allow_undeclared_parameters = False, automatically_declare_parameters_from_overrides = False, enable_logger_service = False):
         super().__init__(node_name, context=context, cli_args=cli_args, namespace=namespace, use_global_arguments=use_global_arguments, enable_rosout=enable_rosout, start_parameter_services=start_parameter_services, parameter_overrides=parameter_overrides, allow_undeclared_parameters=allow_undeclared_parameters, automatically_declare_parameters_from_overrides=automatically_declare_parameters_from_overrides, enable_logger_service=enable_logger_service)
 
+
+        rclpy.get_default_context().on_shutdown(self.cleanup)
+
         # LCD Address (from your i2cdetect): 0x27
         self.LCD_ADDR = 0x27
 
@@ -78,12 +81,19 @@ class LCS_DISP_DRIVER(Node):
         yaw_string = str(round(angular.z, 2))
         self.lcd_string((x_string + "YAW:" + yaw_string), 2)
 
+    def cleanup(self):
+        self.lcd_string("", 1)
+        self.lcd_string("", 2)
+
 
 def main(ars=Node):
     rclpy.init() 
     lcd_disp_driver = LCS_DISP_DRIVER("lcd_disp_driver")  # Pass gamepad instance
-    rclpy.spin(lcd_disp_driver)
-    rclpy.shutdown()
+    try:
+        rclpy.spin(lcd_disp_driver)
+    finally:
+        lcd_disp_driver.cleanup()
+        rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
